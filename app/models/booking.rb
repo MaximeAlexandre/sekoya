e04445d8b224260ec6d07a0b0af934914db60ca2
Anthony Manto <anthony.manto8@gmail.com>
2020-08-18 14:18:44 +0200

    bookin model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :senior, class_name: "User"
   has_many :reviews
   has_many :favoris
+
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :status, presence: true
+  validates :task, presence: true
 end
