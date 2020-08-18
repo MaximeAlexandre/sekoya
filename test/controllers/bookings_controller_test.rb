60e7ef16d7bd26eb1e017ba5a298c4fda8df5b51
MalgouyreBGA <64103534+MalgouyreBGA@users.noreply.github.com>
2020-08-18 14:48:22 +0200

    Update bookings_controller_test.rb

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new:," do
-    get bookings_new:,_url
+  test "should get new" do
+    get bookings_new_url
     assert_response :success
   end
 
-  test "should get create:," do
-    get bookings_create:,_url
+  test "should get create" do
+    get bookings_create_url
     assert_response :success
   end
 
-  test "should get show:," do
-    get bookings_show:,_url
+  test "should get show" do
+    get bookings_show_url
     assert_response :success
   end
 
-  test "should get update:" do
-    get bookings_update:_url
+  test "should get update" do
+    get bookings_update_url
     assert_response :success
   end
 
