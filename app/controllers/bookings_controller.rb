0b9d6fb84149824b066b0642d82f3ea6b9a6c10c
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-18 16:22:12 +0200

    prototype de bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,43 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :start_time, :end_time)
+
+    #  id         :bigint           not null, primary key
+    #  comment    :text
+    #  date       :date
+    #  end_time   :time
+    #  start_time :time
+    #  status     :string
+    #  task       :string
+    #  created_at :datetime         not null
+    #  updated_at :datetime         not null
+    #  helper_id  :bigint           not null
+    #  senior_id  :bigint           not null
+  end
+
 end
