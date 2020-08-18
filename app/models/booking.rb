1e6d4d339b880f9e309a14c4e725592fec83336a
Anthony Manto <64486210+anthomanto@users.noreply.github.com>
2020-08-18 14:25:03 +0200

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,4 +9,13 @@ class Booking < ApplicationRecord
   validates :end_time, presence: true
   validates :status, presence: true
   validates :task, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "must be after the start date") if end_date < start_date
+  end
 end
