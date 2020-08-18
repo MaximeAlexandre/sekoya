db31edcee2d558adb779269eae12fe71b8a149d2
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-18 19:01:38 +0200

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'profiles', to: 'profiles#helper_list'
+  get 'profiles/:id', to: 'profiles#helper_details'
 
   resources :bookings, only: [:new, :create, :show, :update]
-
 end
