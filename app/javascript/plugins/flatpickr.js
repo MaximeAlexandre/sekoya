c7a35d2c3e7fac5074bcc154174eaba1ed2f90a3
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-19 17:32:05 +0200

    show aidant ok with flatpickr and time select

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,11 +2,5 @@ import flatpickr from "flatpickr";
 
 
 flatpickr('.datepicker', {
-  inline: true,
-  enableTime: true,
-  minuteIncrement: 30,
-  time_24hr: true,
-  defaultDate: "7:00",
-  minTime: "7:00",
-  maxTime: "21:00"
+  inline: true
 });
