500eda568790d898d7569e5874cf02c6702e016d
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-19 12:45:35 +0200

    helper_details without flatpickr

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :helper_list
+  before_action :set_user, only: :helper_details
+  skip_before_action :authenticate_user!, only: [:helper_list, :helper_details]
 
   def helper_list
     session[:address_input] = params[:address_input]
@@ -12,10 +13,28 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def helper_details
+    @registration_duration = registered_for(@helper)
+  end
+
   private
 
   def set_user
     @helper = User.find(params[:id])
-    authorize @helper
+    # authorize @helper
+  end
+
+  def registered_for(helper)
+    registered_for_year = (Date.today - helper.created_at.to_date).to_i / 365
+    registered_for_month = (Date.today.month - helper.created_at.to_date.month)
+    registered_for_month += 12 if registered_for_month.negative?
+
+    if registered_for_year.zero? && registered_for_month.zero?
+      "moins de 1 mois"
+    elsif registered_for_year.zero?
+      "#{registered_for_month} mois"
+    else
+      "plus de #{registered_for_year} ans"
+    end
   end
 end
