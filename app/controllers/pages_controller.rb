21aaf461eb3815da157b3881727485a19ae01d8d
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-20 15:32:32 +0200

    4 kind of times

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,43 @@ class PagesController < ApplicationController
   def home
     @address = ""
     @address = current_user.address if user_signed_in?
+    raise
   end
 
   def senior
-    @bookings = Booking.all.where(senior_id: current_user.id).order(start_time: :desc)
-    @bookings.empty? ? @booking_next = false : @booking_next = @bookings.first
+    @bookings = Booking.all.where(senior_id: current_user.id).order(date: :asc, start_time: :asc)
+    @booking_next = @bookings.first unless @bookings.empty?
+    @senior_week = senior_week(@bookings)
+    @en_attente = pending(@bookings)
+    @avenir = futur(@bookings)
   end
 
   def helper
-    @bookings = Booking.all.where(helper_id: current_user.id).order(start_time: :desc)
-    @bookings.empty? ? @booking_next = false : @booking_next = @bookings.first
+    @bookings = Booking.all.where(helper_id: current_user.id).order(date: :asc, start_time: :asc)
+    @booking_next = @bookings.first unless @bookings.empty?
+    @helper_day = helper_day(@bookings)
+    @en_attente = pending(@bookings)
+    @avenir = futur(@bookings)
   end
 
   private
+
+  def helper_day(list)
+    list.where(date: Date.today)
+  end
+
+  def senior_week(list)
+    week = [Date.today]
+    (1..6).each { |i| week << Date.today + i }
+    list.where(date: week)
+  end
+
+  def pending(list)
+    list.where(status: "pending")
+  end
+
+  def futur(list)
+    list.where("date >= ?", Date.today)
+  end
 
 end
