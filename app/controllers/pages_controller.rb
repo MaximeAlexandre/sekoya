7eff1ad1079e9fe120660e636ccc42aeb9405784
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-20 12:27:02 +0200

    prototype dashbord + navbar connect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,17 @@ class PagesController < ApplicationController
     @address = ""
     @address = current_user.address if user_signed_in?
   end
+
+  def senior
+    @bookings = Booking.all.where(senior_id: current_user.id).order(start_time: :desc)
+    @bookings.empty? ? @booking_next = false : @booking_next = @bookings.first
+  end
+
+  def helper
+    @bookings = Booking.all.where(helper_id: current_user.id).order(start_time: :desc)
+    @bookings.empty? ? @booking_next = false : @booking_next = @bookings.first
+  end
+
+  private
+
 end
