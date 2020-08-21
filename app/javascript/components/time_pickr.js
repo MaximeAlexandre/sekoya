dcb5ef057e9dce607ed535e2b6e592ca639fd455
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-21 16:53:50 +0200

    endtime always > starttime

--- app/javascript/components/time_pickr.js
+++ app/javascript/components/time_pickr.js
@@ -1,19 +1,20 @@
 const changeDuration = () => {
-    const startHour = Number(document.getElementsByName("start_time")[0].value)
-    const endHour = Number(document.getElementsByName("end_time")[0].value)
-    const duree = endHour - startHour
-
-    const totalDuration = document.getElementById("total-duration")
-    const price = Number(totalDuration.dataset.price)
-    totalDuration.innerText = ` ${price} € x ${duree} heures `
-
-    const totalPrice = document.getElementById("total-price")
-    totalPrice.innerText =`Total : ${duree*price} €`
+    const startHour = Number(document.getElementsByName("start_time")[0].value);
+    const endHour = document.getElementsByName("end_time")[0];
+    let endHourUse = Number(endHour.value);
+    if (endHourUse <= startHour) {
+        endHourUse = startHour + 1
+        endHour.value = String(endHourUse);
+    }
+    const duree = endHourUse - startHour;
+    const total = document.getElementById("total");
+    const price = Number(total.dataset.price);
+    total.innerText = `${duree} heure - Total : ${duree*price} €`;
 }
 
 const initDuration = () => {
-    const start = document.getElementsByName("start_time")[0]
-    const end = document.getElementsByName("end_time")[0]
+    const start = document.getElementsByName("start_time")[0];
+    const end = document.getElementsByName("end_time")[0];
     start.addEventListener("change", changeDuration);
     end.addEventListener("change", changeDuration);
 }
