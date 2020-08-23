0553ea1c4bfcfee22e6a1c7b9298f9011348d9fd
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-23 20:25:20 +0200

    favoris fondation

--- app/controllers/favoris_controller.rb
+++ app/controllers/favoris_controller.rb
@@ -1,2 +1,20 @@
 class FavorisController < ApplicationController
+    def create      
+        @favori = Favori.new
+        @favori.booking = Booking.find(params[:booking])
+        @favori.save
+        redirect_to senior_path
+    end
+
+    def index
+        @favoris = Booking.joins(:favoris).where(senior: current_user)
+    end
+
+    def destroy
+        booking = Booking.find(params[:id])
+        favori = Favori.find_by(booking: booking)
+        favori.destroy
+        redirect_to favoris_index_path
+    end
+
 end
