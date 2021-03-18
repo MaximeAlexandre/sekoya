840bfca7180f1905aad4fb3f90aeeefaed8bec08
bastien malgouyres <bmalgouyres@free.fr>
2021-03-18 13:31:57 +0100

    fonctions calendar moved to private

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
-  def show
+  def show    
     @sch_begin = Time.now
     @sch_end = @sch_begin + 3600 * 24 * 60
     @sch_duration = @sch_begin + 3600
@@ -29,101 +29,6 @@ class CalendarsController < ApplicationController
     @list_disponibility_classed = data_hashes(@busy, @vs)
     @calendrier = calendrier(@list_disponibility_classed)
   end
-
-  # calendar data start
-  def day_format_string(day_in)
-    day_in.month < 10 ? month = "0#{day_in.month}" : month = "#{day_in.month}"
-    day_in.day < 10 ? day = "0#{day_in.day}" : day = "#{day_in.day}"
-    return "#{day_in.year}-" + month + "-" + day
-  end
-
-  def calendrier(data_list)
-    start_sch = data_list[data_list.keys[0]].keys[0].to_date
-    start_date = start_sch
-    until "#{start_sch.year}-#{start_sch.cweek}" != "#{(start_date - 1.day).year}-#{(start_date - 1.day).cweek}"
-      start_date -= 1.day
-    end
-    start_date = start_sch
-    end_sch = data_list[data_list.keys[-1]].keys[-1].to_date
-    end_date = end_sch
-    until "#{end_sch.year}-#{end_sch.cweek}" != "#{(end_date + 1.day).year}-#{(end_date + 1.day).cweek}"
-      end_date += 1.day
-    end
-    schedule = IceCube::Schedule.new(start = start_date, :end_time => end_date) do |s| # 
-      s.add_recurrence_rule IceCube::Rule.daily
-      .hour_of_day(0)
-      .minute_of_hour(0)
-      .second_of_minute(0)
-    end
-    schedule_classed = {}
-    schedule.occurrences(end_date).each do |day|
-      key="#{day.year}-#{day.to_date.cweek}"
-      if schedule_classed[key].nil?
-        schedule_classed[key] = [day_format_string(day)]
-      else
-        schedule_classed[key] << day_format_string(day)
-      end
-    end
-    return schedule_classed
-  end
-
-  def list_filling(data, name)
-    l = {}
-    data.each do |sch|
-      if sch.class == Array
-        first_key_a = "#{sch[0].to_date.year}-#{sch[0].to_date.cweek}"
-        if l[first_key_a] == nil
-          l[first_key_a] = { "#{sch[0].to_date}" => {name => [[sch[0].hour, sch[1]]]} }
-        else
-          if l[first_key_a]["#{sch[0].to_date}"] == nil
-            l[first_key_a]["#{sch[0].to_date}"] = {name => [[sch[0].hour, sch[1]]]}
-          else
-            l[first_key_a]["#{sch[0].to_date}"][name] << [sch[0].hour, sch[1]]
-          end
-        end
-      else
-        first_key_s = "#{sch.to_date.year}-#{sch.to_date.cweek}"
-        if l[first_key_s] == nil
-          l[first_key_s] = { "#{sch.to_date}" => {name => [sch.hour]}}
-        else
-          if l[first_key_s]["#{sch.to_date}"] == nil
-            l[first_key_s]["#{sch.to_date}"] = {name => [sch.hour]}
-          else
-            l[first_key_s]["#{sch.to_date}"][name] << sch.hour
-          end
-        end
-      end
-    end
-    return l
-  end
-
-  def data_hashes(busy, free)
-    list_free = list_filling(free, "free")
-    list_busy = list_filling(busy, "busy")
-    list = {}
-    keys_list_week = (list_free.keys | list_busy.keys)
-    keys_list_week.each do |key_week|
-      if list_free[key_week] == nil
-        list[key_week] = list_busy[key_week]
-      elsif list_busy[key_week] == nil
-        list[key_week] = list_free[key_week]
-      else
-        keys_list_days = (list_free[key_week].keys | list_busy[key_week].keys)
-        list[key_week] = {}
-        keys_list_days.each do |key_day|
-          if list_free[key_week][key_day] == nil
-            list[key_week][key_day] = list_busy[key_week][key_day]
-          elsif list_busy[key_week][key_day] == nil
-            list[key_week][key_day] = list_free[key_week][key_day]
-          else
-            list[key_week][key_day] = list_free[key_week][key_day].merge(list_busy[key_week][key_day])
-          end
-        end
-      end
-    end
-    return list
-  end
-  # calendar data end
 
   def form
     @sch_begin = Time.now
@@ -281,4 +186,97 @@ class CalendarsController < ApplicationController
     end
   end
 
+  # calendar data start
+  def day_format_string(day_in)
+    day_in.month < 10 ? month = "0#{day_in.month}" : month = "#{day_in.month}"
+    day_in.day < 10 ? day = "0#{day_in.day}" : day = "#{day_in.day}"
+    return "#{day_in.year}-" + month + "-" + day
+  end
+
+  def calendrier(data_list)
+    start_sch = data_list[data_list.keys[0]].keys[0].to_date
+    start_date = start_sch
+    until "#{start_sch.year}-#{start_sch.cweek}" != "#{(start_date - 1.day).year}-#{(start_date - 1.day).cweek}"
+      start_date -= 1.day
+    end
+    start_date = start_sch
+    end_sch = data_list[data_list.keys[-1]].keys[-1].to_date
+    end_date = end_sch
+    until "#{end_sch.year}-#{end_sch.cweek}" != "#{(end_date + 1.day).year}-#{(end_date + 1.day).cweek}"
+      end_date += 1.day
+    end
+    schedule = IceCube::Schedule.new(start = start_date, :end_time => end_date) do |s| # 
+      s.add_recurrence_rule IceCube::Rule.daily
+      .hour_of_day(0)
+      .minute_of_hour(0)
+      .second_of_minute(0)
+    end
+    schedule_classed = {}
+    schedule.occurrences(end_date).each do |day|
+      key="#{day.year}-#{day.to_date.cweek}"
+      if schedule_classed[key].nil?
+        schedule_classed[key] = [day_format_string(day)]
+      else
+        schedule_classed[key] << day_format_string(day)
+      end
+    end
+    return schedule_classed
+  end
+
+  def list_filling(data, name)
+    l = {}
+    data.each do |sch|
+      if sch.class == Array
+        first_key_a = "#{sch[0].to_date.year}-#{sch[0].to_date.cweek}"
+        if l[first_key_a] == nil
+          l[first_key_a] = { "#{sch[0].to_date}" => {name => [[sch[0].hour, sch[1]]]} }
+        else
+          if l[first_key_a]["#{sch[0].to_date}"] == nil
+            l[first_key_a]["#{sch[0].to_date}"] = {name => [[sch[0].hour, sch[1]]]}
+          else
+            l[first_key_a]["#{sch[0].to_date}"][name] << [sch[0].hour, sch[1]]
+          end
+        end
+      else
+        first_key_s = "#{sch.to_date.year}-#{sch.to_date.cweek}"
+        if l[first_key_s] == nil
+          l[first_key_s] = { "#{sch.to_date}" => {name => [sch.hour]}}
+        else
+          if l[first_key_s]["#{sch.to_date}"] == nil
+            l[first_key_s]["#{sch.to_date}"] = {name => [sch.hour]}
+          else
+            l[first_key_s]["#{sch.to_date}"][name] << sch.hour
+          end
+        end
+      end
+    end
+    return l
+  end
+
+  def data_hashes(busy, free)
+    list_free = list_filling(free, "free")
+    list_busy = list_filling(busy, "busy")
+    list = {}
+    keys_list_week = (list_free.keys | list_busy.keys)
+    keys_list_week.each do |key_week|
+      if list_free[key_week] == nil
+        list[key_week] = list_busy[key_week]
+      elsif list_busy[key_week] == nil
+        list[key_week] = list_free[key_week]
+      else
+        keys_list_days = (list_free[key_week].keys | list_busy[key_week].keys)
+        list[key_week] = {}
+        keys_list_days.each do |key_day|
+          if list_free[key_week][key_day] == nil
+            list[key_week][key_day] = list_busy[key_week][key_day]
+          elsif list_busy[key_week][key_day] == nil
+            list[key_week][key_day] = list_free[key_week][key_day]
+          else
+            list[key_week][key_day] = list_free[key_week][key_day].merge(list_busy[key_week][key_day])
+          end
+        end
+      end
+    end
+    return list
+  end
 end
